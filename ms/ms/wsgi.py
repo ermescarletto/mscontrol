a63d2b16db90c40387741bb145dc6a323963502f
Ermes Carletto Junior <ermes.carletto@gmail.com>
2022-06-30 16:31:28 -0300

    Corrigido o bug na geração do qrcodpythone

--- ms/ms/wsgi.py
+++ ms/ms/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/opt/bitnami/projects/mscontrol/ms/')
+os.environ.setdefault('PYTHON_EGG_CACHE', '/opt/bitnami/projects/mscontrol/ms/egg_cache')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ms.settings')
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
--- ms/servicos/models.py
+++ ms/servicos/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class TipoServico(models.Model):
     UNIDADES = [
         ('UN', 'UNIDADE'),
